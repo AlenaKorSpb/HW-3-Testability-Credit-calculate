8257260400692f3a9e441cc46b196ffb3dc86fa6
AlenaKorSpb <evtam-29@mail.ru>
2020-09-17 23:25:53 +0300

    change commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,13 @@
 public class CreditPaymentService {
-    public double calculait (double amount, int time, int percent) {
-        double totalmouns = amount / time + (amount/100.0*(percent/12.0/100.0));
-        return totalmouns;
+    public double calculait(double amountCredit, int timeMounth, float percentYear) {
+        double percentMounth = percentYear / 12 / 100;
+        double k = (percentMounth * Math.pow((1 + percentMounth), timeMounth)) / (Math.pow(1 + percentMounth, timeMounth) - 1);
+        double paymentMounth = k * amountCredit;
+
+        return paymentMounth;
+
     }
 }
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService creditPaymentService = new CreditPaymentService();
-        double totalmouns12 = creditPaymentService .calculait(1000000, 12, (int) 9.99);
-        System.out.println(totalmouns12);
+        double totalMounth12 = creditPaymentService.calculait(1000000.0, 12, (float) 9.99);
+        System.out.println(totalMounth12);
 
-        double totalmouns24 = creditPaymentService .calculait(1000000, 24, (int) 9.99);
-        System.out.println(totalmouns24);
+        double totalMounth24 = creditPaymentService.calculait(1000000.0, 24, (float) 9.99);
+        System.out.println(totalMounth24);
 
-        double totalmouns36 = creditPaymentService .calculait(1000000, 36, (int) 9.99);
-        System.out.println(totalmouns36);
+        double totalMounth36 = creditPaymentService.calculait(1000000.0, 36, (float) 9.99);
+        System.out.println(totalMounth36);
     }
 }
